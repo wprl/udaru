654dbc9387b10235914f774a17e37cb982bd4a74
Mihai Dima <mihaizn@gmail.com>
2017-09-26 15:44:07 +0300

    test: moved tests into authoriztion, removed unneeded files

--- node2pino-colada.sh
+++
@@ -1,1 +0,0 @@
-node $* | pino-colada
--- test/integration/endToEnd/authorization.test.js
+++ test/integration/endToEnd/authorization.test.js
@@ -5,6 +5,7 @@ const Lab = require('lab')
 const lab = exports.lab = Lab.script()
 const utils = require('../../utils')
 const server = require('../../../lib/server')
+const Factory = require('../../factory')
 
 lab.experiment('Authorization', () => {
   lab.test('check authorization should return access true for allowed', (done) => {
@@ -105,3 +106,123 @@ lab.experiment('Authorization', () => {
     })
   })
 })
+
+lab.experiment('Authorization root user in impersonated organization', () => {
+  const NewOrgPolicyId = 'NewOrgPolicyId'
+  const NewOrgId = 'NewOrgId'
+
+  Factory(lab, {
+    organizations: {
+      org1: {
+        id: NewOrgId,
+        name: 'New Organization.',
+        description: 'The Mighty New Organization.',
+        policies: ['org1AdminPolicy']
+      }
+    },
+    users: {
+      user1: {
+        id: 'AdminId',
+        name: 'Mr. Admin No Policy',
+        organizationId: 'ROOT'
+      }
+    },
+    policies: {
+      org1AdminPolicy: {
+        id: NewOrgPolicyId,
+        name: 'NewOrgPolicyId',
+        organizationId: NewOrgId,
+        statements: {
+          Statement: [
+            {
+              Effect: 'Allow',
+              Action: ['do:stuff'],
+              Resource: ['all:stuff:*']
+            }
+          ]
+        }
+      }
+    }
+  })
+
+  lab.test('The AdminId user and its organisation have no policies attached', (done) => {
+    const options = utils.requestOptions({
+      method: 'GET',
+      url: '/authorization/users/AdminId',
+      headers: {
+        authorization: 'ROOTid'
+      }
+    })
+
+    server.inject(options, (response) => {
+      expect(response.statusCode).to.equal(200)
+      expect(response.result.policies).to.have.length(0)
+
+      options.url = '/authorization/organizations/ROOT'
+      server.inject(options, (response) => {
+        expect(response.statusCode).to.equal(200)
+        expect(response.result.policies).to.have.length(0)
+
+        done()
+      })
+    })
+  })
+
+  lab.test('The NewOrgId organisation has one policy attached', (done) => {
+    const options = utils.requestOptions({
+      method: 'GET',
+      url: `/authorization/organizations/${NewOrgId}`,
+      headers: {
+        authorization: 'ROOTid'
+      }
+    })
+
+    server.inject(options, (response) => {
+      expect(response.statusCode).to.equal(200)
+      expect(response.result.policies[0].id).to.equal(NewOrgPolicyId)
+
+      done()
+    })
+  })
+
+  lab.test('A user should not inherit a list of actions from a policy assigned to an organization it does not belong to', (done) => {
+    const options = utils.requestOptions({
+      method: 'GET',
+      url: '/authorization/list/AdminId/all:stuff:*',
+      headers: {
+        authorization: 'ROOTid',
+        org: NewOrgId
+      }
+    })
+
+    server.inject(options, (response) => {
+      const result = response.result
+
+      expect(response.statusCode).to.equal(200)
+      expect(result.actions).to.have.length(0)
+
+      done()
+    })
+  })
+
+  lab.test('A user should not inherit access to a resource from a policy assigned to an organization it does not belong to', (done) => {
+    const options = utils.requestOptions({
+      method: 'GET',
+      url: '/authorization/access/AdminId/do:stuff/all:stuff:*',
+      headers: {
+        authorization: 'ROOTid',
+        org: NewOrgId
+      }
+    })
+
+    server.inject(options, (response) => {
+      const result = response.result
+
+      expect(response.statusCode).to.equal(200)
+      expect(result.access).to.equal(false)
+
+      done()
+    })
+  })
+})
+
